e143d6779dcf8b00473172199efe617379cb339d
Azeem Khan <azeemkhanreal@gmail.com>
2022-07-12 18:50:22 +0530

    removed: unnecceary vendor routes

--- routes/VendorRoute.ts
+++ routes/VendorRoute.ts
@@ -4,10 +4,6 @@ import { Authenticate } from "../middlewares";
 const router = express.Router();
 
 router.post("/login",VendorLogin)
-router.get("/profile",Authenticate,GetVendorProfile)
-router.patch("/profile",UpdateVendorProfile)
-router.patch("/profile",UpdateVendorService)
-
 
 router.use(Authenticate)
 
